f41be79c2c244e7b8bc0b7caf8b27a647f59a116
+ <familymanjosh@gmail.com>
2022-12-06 17:06:29 -0800

    commit tree 092772075aa46effb7334bd9386206fbf09b7214

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(a,b) {
+return a+b;
+}
+function subtract(a,b) {
+    return a-b;
+}
+function multiply(a,b) {
+    return a*b;
+}
+function divide(a,b) {
+    return a/b;
+}
+function increment(n) {
+ let a=n 
+    a++
+return a;
+}
+function decrement(n){
+    let a=n
+    a--
+    return a;
+}
+function makeInt(n){
+    return parseInt(n,10)
+}
+function preserveDecimal(n){
+    return parseFloat(n)
+}
